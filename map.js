dd9ca215126e501eff784df9adb4cd53bb4711ba
Gao <xingaoemail@163.com>
2021-11-08 20:15:18 +0800

    feat: hash start

--- map.js
+++ map.js
@@ -1,5 +1,14 @@
 class GMap {
   constructor () {
-    this.map = []
+    this.size(0)
+    this.bucket = new Array(8)
+  }
+
+  hash(key) {
+    let index
+    if (typeof key === 'string') {
+      index = 0
+    }
+    return index
   }
 }
