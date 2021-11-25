2a0f6d127497eae277e21f974f4d2f30b0ad920c
Gao <xingaoemail@163.com>
2021-11-25 17:07:43 +0800

    feat: diff array

---
+++ diff/index.js
@@ -0,0 +1,7 @@
+const diffArray = (array1, array2, { mountElement, unmount, patch, move }) => {
+  const len1 = array1.length
+  const len2 = array2.length
+
+  const end1 = len1 - 1
+  const end2 = len2 - 1
+}
