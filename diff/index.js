172579c0528b242cc2254ec9ac54017a85229900
Gao <xingaoemail@163.com>
2021-11-26 14:32:43 +0800

    feat: 左侧校验

--- diff/index.js
+++ diff/index.js
@@ -1,7 +1,21 @@
+// array1：旧数组，array2：新数组
 const diffArray = (array1, array2, { mountElement, unmount, patch, move }) => {
+  const isSameVnodeType = (n1, n2) => {
+    return n1.key === n2.key && n1.type === n2.type
+  }
+
   const len1 = array1.length
   const len2 = array2.length
 
   const end1 = len1 - 1
   const end2 = len2 - 1
+
+  let i = 0
+  // ! 双端校验
+  // * 1.从首端（左边）开始按序查找，直到不匹配
+  while (i < end1 && 1 < end2) {
+    const node1 = array1[i]
+    const node2 = array2[i]
+    if (isSameVnodeType(node1, node2)) i++
+  }
 }
