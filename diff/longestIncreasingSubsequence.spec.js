44ff5c9503e576532d39ec2e4f2c29960950e1e5
Gao <xingaoemail@163.com>
2021-12-01 21:16:18 +0800

    feat: 右侧查找测试用例

--- diff/longestIncreasingSubsequence.spec.js
+++ diff/longestIncreasingSubsequence.spec.js
@@ -19,4 +19,24 @@ describe('Array Diff', () => {
     expect(patch.mock.calls[0][0]).toBe('a')
     expect(patch.mock.calls[1][0]).toBe('b')
   })
+  it('2. 右边查找', () => {
+    const mountElement = jest.fn()
+    const unmount = jest.fn()
+    const patch = jest.fn()
+    const move = jest.fn()
+    const { diffArray } = require('./longestIncreasingSubsequence')
+    diffArray(
+      [{ key: 'a' }, { key: 'b' }, { key: 'c' }],
+      [{ key: 'd' }, { key: 'e' }, { key: 'b' }, { key: 'c' }],
+      {
+        mountElement,
+        unmount,
+        patch,
+        move
+      }
+    )
+    expect(patch.mock.calls.length).toBe(2)
+    expect(patch.mock.calls[0][0]).toBe('c')
+    expect(patch.mock.calls[1][0]).toBe('b')
+  })
 })
