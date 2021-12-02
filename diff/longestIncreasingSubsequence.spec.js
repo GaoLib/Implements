6fcedffa33ffb6e568165727adb2a2f0e8fdda55
Gao <xingaoemail@163.com>
2021-12-02 18:05:23 +0800

    feat: 新增节点测试用例

--- diff/longestIncreasingSubsequence.spec.js
+++ diff/longestIncreasingSubsequence.spec.js
@@ -39,4 +39,24 @@ describe('Array Diff', () => {
     expect(patch.mock.calls[0][0]).toBe('c')
     expect(patch.mock.calls[1][0]).toBe('b')
   })
+  it('3. 旧节点没了，新节点还有', () => {
+    const mountElement = jest.fn()
+    const unmount = jest.fn()
+    const patch = jest.fn()
+    const move = jest.fn()
+    const { diffArray } = require('./longestIncreasingSubsequence')
+    diffArray(
+      [{ key: 'a' }, { key: 'b' }],
+      [{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }],
+      {
+        mountElement,
+        unmount,
+        patch,
+        move
+      }
+    )
+    expect(mountElement.mock.calls.length).toBe(2)
+    expect(mountElement.mock.calls[0][0]).toBe('c')
+    expect(mountElement.mock.calls[1][0]).toBe('d')
+  })
 })
