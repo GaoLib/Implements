635c24088e9e6288c1ce11b4dd0a8cd54bdc0994
Gao <xingaoemail@163.com>
2021-12-13 15:34:18 +0800

    feat: 遍历旧的Vdom，判断更新还是卸载

--- diff/longestIncreasingSubsequence.js
+++ diff/longestIncreasingSubsequence.js
@@ -25,28 +25,28 @@ const diffArray = (child1, child2, { mountElement, unmount, patch, move }) => {
   }
   // * 2.从末端（右边）开始按序查找，直到不匹配
   while (i <= end1 && i <= end2) {
-    const node1 = child1[e1]
-    const node2 = child2[e2]
+    const node1 = child1[end1]
+    const node2 = child2[end2]
     if (isSameVnodeType(node1, node2)) {
       patch(node1.key)
     } else {
       break
     }
-    e1--
-    e2--
+    end1--
+    end2--
   }
   // * 3.旧节点没了，新节点还有
-  if (i > e1) {
-    if (i <= e2) {
-      while (i <= e2) {
+  if (i > end1) {
+    if (i <= end2) {
+      while (i <= end2) {
         const node = child2[i]
         mountElement(node.key)
         i++
       }
     }
   // * 4.新节点没了，旧节点还有
-  } else if (i > e2) {
-    while (i <= e1) {
+  } else if (i > end2) {
+    while (i <= end1) {
       const node = child1[i]
       unmount(node.key)
       i++
@@ -55,11 +55,21 @@ const diffArray = (child1, child2, { mountElement, unmount, patch, move }) => {
   } else {
     const start1 = i
     const start2 = i
-    // ! 新的VDOM做成Map,方便查找，修改
+    // ! 1. 新的VDOM做成Map,方便查找，修改
     const keyToNewIndexMap = new Map()
-    for (i = start2; i <= e2; i++) {
+    for (i = start2; i <= end2; i++) {
       const node = child2[i]
       keyToNewIndexMap.set(node.key, i)
+    }
+    // ! 2. 遍历旧的VDOM，在Map中找到则patch，否则unmount
+    for (i = start1; i <= end1; i++) {
+      const prevChild = child1[i]
+      let newIndex = keyToNewIndexMap.get(prevChild.key)
+      if (newIndex === undefined) {
+        unmount(prevChild.key)
+      } else {
+        patch(prevChild.key)
+      }
     }
   }
 }
